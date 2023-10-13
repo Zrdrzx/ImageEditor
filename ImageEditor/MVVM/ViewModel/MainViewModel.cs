79a34be45595139d0f1d869e9ecc695705f67709
Zrdrzx <129935328+Zrdrzx@users.noreply.github.com>
2023-10-13 10:09:51 +0200

    VieModel class, Changes in MainViewModel class and in App.xaml

--- ImageEditor/MVVM/ViewModel/MainViewModel.cs
+++ ImageEditor/MVVM/ViewModel/MainViewModel.cs
@@ -9,9 +9,21 @@ namespace ImageEditor.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        public HomeViewModel HomeVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
         public MainViewModel()
         {
-
+            HomeVM = new HomeViewModel();
         }
 
     }
