89a8e025145336091151ad46332a3d2aa5bebe0e
Zrdrzx <129935328+Zrdrzx@users.noreply.github.com>
2023-10-13 09:12:08 +0200

    Created HomeView and MainViewModel class

---
+++ ImageEditor/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ImageEditor.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageEditor.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+
+    }
+}
